af5a8f640ce3b264ef7fd52e04110de1bae93b84
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-10-16 11:16:37 +0200

    API call setup to get location suggestions

--- weather_application/src/components/WeatherSearch.tsx
+++ weather_application/src/components/WeatherSearch.tsx
@@ -1,4 +1,24 @@
-const WeatherSearch = ():JSX.Element => {
+import {useState, ChangeEvent} from 'react';
+import { LocationSuggestion } from '../types/types';
+import { get } from 'http';
+
+const WeatherSearch = ():JSX.Element =>{
+const API_BASE_URL: string = `http://api.openweathermap.org/geo/1.0/direct`;
+const [searchField, setSearchField] = useState<string>('');
+const [locationSuggestions, setLocationSuggestions] = useState<LocationSuggestion[]>([]);
+
+const onInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchField(event.target.value);
+    getSuggestions(event.target.value);
+}
+
+const getSuggestions = async (searcField: string) => {
+    fetch(`${API_BASE_URL}?q=${searcField}&limit=5&appid=${process.env.REACT_APP_APIKEY}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    
+    .catch((error) => console.log(error));
+}
     return(
         <div className="lg:h-[500px] lg:w-[600px] p-24 flex flex-col justify-center items-center bg-red-600">
         <h1 className='text-4xl'>
@@ -8,7 +28,7 @@ const WeatherSearch = ():JSX.Element => {
         Search for a place to find the current weather conditions.
         </p>
         <div className="flex">
-        <input type='text' className='px-2'/>
+        <input type='text' className='px-2' onInput={onInput}/>
         <button className='cursor-pointer border-solid border-light-blue-500'>
         Search
         </button>
