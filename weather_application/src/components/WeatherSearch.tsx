13a8901f2c2d6dcea9e89edb1422d3fac651f430
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-10-16 11:47:18 +0200

    Fetching weather info about location and interface refactoring

--- weather_application/src/components/WeatherSearch.tsx
+++ weather_application/src/components/WeatherSearch.tsx
@@ -1,11 +1,13 @@
 import {useState, ChangeEvent} from 'react';
-import { LocationSuggestion } from '../types/types';
-import { get } from 'http';
+import { Location } from '../types/types';
+import { WeatherData } from '../types/types';
 
 const WeatherSearch = ():JSX.Element =>{
 const API_BASE_URL: string = `http://api.openweathermap.org/geo/1.0/direct`;
+
+const [selectedLocation, setSelectedLocation] = useState<Location | null>(null);
 const [searchField, setSearchField] = useState<string>('');
-const [locationSuggestions, setLocationSuggestions] = useState<LocationSuggestion[]>([]);
+const [locationSuggestions, setLocationSuggestions] = useState<Location[]>([]);
 
 const onInput = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchField(event.target.value);
@@ -19,6 +21,38 @@ const getSuggestions = async (searchField: string) => {
     
     .catch((error) => console.log(error));
 }
+const onSuggestionClick = (suggestion: Location) => {
+    console.log(suggestion.name);
+    setSearchField(suggestion.name);
+    setSelectedLocation(suggestion);
+}
+const onSearchClick = () => {
+    if(selectedLocation === null) return;
+    getWeather(selectedLocation);
+}
+const getWeather = async (selectedLocation: Location) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${selectedLocation.lat}&lon=${selectedLocation.lon}&appid=${process.env.REACT_APP_APIKEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        const weatherData = mapToWeatherData(data);
+        console.log(weatherData);
+    }).catch((error) => console.log(error));
+}
+
+const mapToWeatherData = (data: any): WeatherData => {
+    
+    const temp: WeatherData = {
+        name: data.name,
+        country: data.sys.country,
+        weather: data.weather[0],
+        main: {...data.main}
+    }
+    return temp;
+}
+
+
+
     return(
         <div className="lg:h-[500px] lg:w-[600px] p-24 flex flex-col justify-center items-center bg-red-600">
         <h1 className='text-4xl'>
@@ -30,15 +64,15 @@ const getSuggestions = async (searchField: string) => {
         <div className="flex relative">
         <input type='text' className='px-2' value={searchField} onInput={onInput}/>
         <ul className='absolute top-7'>
-        {locationSuggestions.map((suggestion: LocationSuggestion, index: number) => (
+        {locationSuggestions.map((suggestion: Location, index: number) => (
         <li key={index}> 
-        <button>
+        <button onClick={() => onSuggestionClick(suggestion)}>
             {suggestion.name}
         </button> 
         </li>
         ))}
     </ul>
-        <button className='cursor-pointer border-solid border-light-blue-500'>
+        <button className='cursor-pointer' onClick={onSearchClick} >
         Search
         </button>
         </div>
--- weather_application/src/types/types.ts
+++ weather_application/src/types/types.ts
@@ -1,3 +1,27 @@
-export interface LocationSuggestion {
+export interface Location {
     name: string,
+    country: string,
+    lat: number,
+    lon: number
+}
+
+export interface WeatherData {
+    name: string,
+    country: string,
+    weather: Weather,
+    main: Main,
+}
+
+interface Main {
+    feels_like: number,
+    humidity: number,
+    temp: number,
+    temp_max: number,
+    temp_min: number
+}
+interface Weather{
+    description: string,
+    icon: string,
+    id:number,
+    main: string
 }
